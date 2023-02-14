dd8dc960e489ee576309c088a6bb822f9fd45563
Jirachiii <trangdy1112@gmail.com>
2023-02-14 15:17:10 +0700

    Create main.py

---
+++ src-2.0/main.py
@@ -0,0 +1,34 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="This is just a description")
+parser.add_argument('-m', '--model', action='store', help="model's name", required=False)
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-d', '--data', action='store_true', help='data preprocessing')
+group.add_argument('-t', '--train', action='store_true', help='train model')
+group.add_argument('-e', '--evaluate', action='store_true', help='evalute model')
+args = parser.parse_args()
+
+if args.data:
+    print("> Processing Data...\n")
+
+    print("> Done!\n")
+
+if args.train:
+    print("> Training...\n")
+
+    print("> Load dataset...\n")
+
+    print("> Initialize model...\n")
+
+    print("> Done!\n")
+
+if args.evaluate:
+    print("> Evaluating...\n")
+
+    print("> Load dataset...\n")
+
+    print("> Initialize model...\n")
+
+    print("> Load pre-trained model...\n")
+        
+    # Result
