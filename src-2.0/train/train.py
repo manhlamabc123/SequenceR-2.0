6516b6409e9e0138a2a1699c43e8769b8d27ef06
Jirachiii <trangdy1112@gmail.com>
2023-02-21 10:19:07 +0700

    Implement Optimizer: stochastic gradient descent

--- src-2.0/train/train.py
+++ src-2.0/train/train.py
@@ -1,21 +1,31 @@
-def per_iter():
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from hyperparameters import *
+
+def per_iter(optimizer: optim.Optimizer):
     # Arguments: model, train_set, optimizer, criterion
     # Variables
     # Loop through train_set
+    for i in range(100):
     ## Source, Target
     ## Clear cache
+        optimizer.zero_grad()
     ## Forward
     ## Compute loss
     ## Update loss
     ## Backward
     ## Update parameters
+        optimizer.step()
     # Return final_loss
     pass
 
-def train():
+def train(model: nn.Module, learning_rate=LEARNING_RATE, momentum=MOMENTUM):
     # Arguments: model, train_set, val_set, learning_rate, epochs, print_every
     # Variables
     # Loss & Optimizer
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
     # Training
     ## Start timer
     ## Make sure gradient tracking is on, and do a pass over the data
