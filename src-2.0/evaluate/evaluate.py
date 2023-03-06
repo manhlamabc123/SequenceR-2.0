60594fe5c771a1fff49a783c7fab60d6a8a48be1
Jirachiii <trangdy1112@gmail.com>
2023-03-06 11:27:20 +0700

    Implement evaluate

--- src-2.0/evaluate/evaluate.py
+++ src-2.0/evaluate/evaluate.py
@@ -1,4 +1,25 @@
 import torch.nn as nn
 
-def evaluate(val_set, model: nn.Module, criterion: nn.NLLLoss):
-    pass
+from constanst import *
+
+def evaluate(data_set, model: nn.Module, criterion: nn.NLLLoss):
+    # Initialize some variables
+    final_loss = 0
+    current_loss = 0
+
+    for i, data in enumerate(data_set):
+        # Extract data from dataset_loader
+        input, target = data # (length, BATCH_SIZE)
+        input, target = input.to(DEVICE), target.to(DEVICE)
+        
+        # Foward
+        output = model(input, target) # (length, BATCH_SIZE, target's dictionary_length)
+
+        # Compute loss
+        loss = criterion(output.view(-1, output.shape[2]), target.view(target.shape[0] * target.shape[1]))
+
+        # Update loss
+        current_loss += loss.item()
+
+    final_loss = current_loss / len(data_set)
+    return final_loss
