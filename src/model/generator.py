ebc0fb15d20408dbdbbc97fe4f1ba2ed876af261
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-04-04 16:22:49 +0700

    Copy copy generator from OpenNMT code

---
+++ src/model/generator.py
@@ -0,0 +1,90 @@
+import torch
+import torch.nn as nn
+
+class CopyGenerator(nn.Module):
+    """An implementation of pointer-generator networks (See et al., 2017)
+    (https://arxiv.org/abs/1704.04368), which consider copying words
+    directly from the source sequence.
+    The copy generator is an extended version of the standard
+    generator that computes three values.
+    * :math:`p_{softmax}` the standard softmax over `tgt_dict`
+    * :math:`p(z)` the probability of copying a word from
+      the source
+    * :math:`p_{copy}` the probility of copying a particular word.
+      taken from the attention distribution directly.
+    The model returns a distribution over the extend dictionary,
+    computed as
+    :math:`p(w) = p(z=1)  p_{copy}(w)  +  p(z=0)  p_{softmax}(w)`
+    .. mermaid::
+       graph BT
+          A[input]
+          S[src_map]
+          B[softmax]
+          BB[switch]
+          C[attn]
+          D[copy]
+          O[output]
+          A --> B
+          A --> BB
+          S --> D
+          C --> D
+          D --> O
+          B --> O
+          BB --> O
+    Args:
+       input_size (int): size of input representation
+       output_size (int): size of output vocabulary
+       pad_idx (int)
+    """
+
+    def __init__(self, input_size, output_size, pad_idx):
+        super(CopyGenerator, self).__init__()
+        self.linear = nn.Linear(input_size, output_size)
+        self.linear_copy = nn.Linear(input_size, 1)
+        self.pad_idx = pad_idx
+
+    def forward(self, hidden, attn, src_map):
+        """
+        Compute a distribution over the target dictionary
+        extended by the dynamic dictionary implied by compying
+        source words.
+        Args:
+           hidden (`FloatTensor`): hidden outputs `[batch*tlen, input_size]`
+           attn (`FloatTensor`): attn for each `[batch*tlen, input_size]`
+           src_map (`FloatTensor`):
+             A sparse indicator matrix mapping each source word to
+             its index in the "extended" vocab containing.
+             `[src_len, batch, extra_words]`
+        """
+        # CHECKS
+        batch_by_tlen, _ = hidden.size()
+        batch_by_tlen_, slen = attn.size()
+        slen_, batch, cvocab = src_map.size()
+        aeq(batch_by_tlen, batch_by_tlen_)
+        aeq(slen, slen_)
+
+        # Original probabilities.
+        logits = self.linear(hidden)
+        logits[:, self.pad_idx] = -float('inf')
+        prob = torch.softmax(logits, 1)
+
+        # Probability of copying p(z=1) batch.
+        p_copy = torch.sigmoid(self.linear_copy(hidden))
+        # Probability of not copying: p_{word}(w) * (1 - p(z))
+        out_prob = torch.mul(prob, 1 - p_copy)
+        mul_attn = torch.mul(attn, p_copy)
+        copy_prob = torch.bmm(
+            mul_attn.view(-1, batch, slen).transpose(0, 1),
+            src_map.transpose(0, 1)
+        ).transpose(0, 1)
+        copy_prob = copy_prob.contiguous().view(-1, cvocab)
+        return torch.cat([out_prob, copy_prob], 1)
+    
+def aeq(*args):
+    """
+    Assert all arguments have the same value
+    """
+    arguments = (arg for arg in args)
+    first = next(arguments)
+    assert all(arg == first for arg in arguments), \
+        "Not all arguments have the same value: " + str(args)
